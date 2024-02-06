5630e44322bc635ec90d85ea7e097187b173543c
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-06 12:18:59 +1100

    lab complted

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+}
+
+let number = 10;
+
+function countDown() {
+    for(let i = number; i >= 0; i--)
+    console.log(i);
+}
+countDown();
